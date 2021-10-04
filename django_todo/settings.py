ebd26e21fb4bd3d4260ea499cc5d1515ff5af85e
michaelhesch <michael.hesch@gmail.com>
2021-10-04 16:11:16 +0000

    update allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mh-hello-django.herokuapp.com']
 
 
 # Application definition
